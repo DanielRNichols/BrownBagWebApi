963c5e27f8ab33475ab67b3154b331089d9187f4
Daniel Nichols <dnichols@gilico.com>
2022-03-08 14:59:39 -0600

    Adds BaseDbRepository

---
+++ NET6.WebApi/Repositories/BaseDbRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using NET6.Shared.Interfaces;
+using Dapper.Contrib.Extensions;
+using System.Data;
+
+namespace NET6.WebApi.Repositories
+{
+    public class BaseDbRepository<T> : IBaseDbRepository<T> where T : class, IDbResource, new()
+    {
+        private readonly Func<IDbConnection> _connectionDelegate;
+
+        public BaseDbRepository(Func<IDbConnection> connectionDelegate)
+        {
+            _connectionDelegate = connectionDelegate;
+        }
+
+        private IDbConnection Connection => _connectionDelegate();
+
+        public async Task<int> AddAsync(T item)
+        {
+            using var dbConn = Connection;
+            var id = await dbConn.InsertAsync(item);
+
+            return id;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            using var dbConn = Connection;
+            var success = await dbConn.DeleteAsync<T>(new T { Id = id });
+
+            return success;
+        }
+
+        public async Task<IList<T>?> GetAllAsync()
+        {
+            using var dbConn = Connection;
+            var items = await dbConn.GetAllAsync<T>();
+
+            return items.ToList();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            using var dbConn = Connection;
+            var item = await dbConn.GetAsync<T>(id);
+
+            return item;
+        }
+
+        public async Task<bool> UpdateAsync(T item)
+        {
+            using var dbConn = Connection;
+            var success = await dbConn.UpdateAsync<T>(item);
+
+            return success;
+        }
+    }
+}
---
+++ NET6.WebApi/Repositories/IBaseDbRepository.cs
@@ -0,0 +1,13 @@
+﻿using NET6.Shared.Interfaces;
+
+namespace NET6.WebApi.Repositories
+{
+    public interface IBaseDbRepository<T> where T : class, IDbResource
+    {
+        Task<IList<T>?> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<int> AddAsync(T item);
+        Task<bool> UpdateAsync(T item);
+        Task<bool> DeleteAsync(int id);
+    }
+}
