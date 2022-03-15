5254ba97ddf777150f934f88e08859caa85d067a
Daniel Nichols <danielrnichols63@gmail.com>
2022-03-15 07:04:45 -0500

    Mocks AutoMapper Adds some MockData

---
+++ NET6.WebApi.Test/MockData/PresentersMockData.cs
@@ -0,0 +1,46 @@
+﻿using NET6.Shared.Dtos;
+using NET6.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET6.WebApi.Test.MockData
+{
+    public class PresentersMockData
+    {
+        public static IList<Presenter> MockPresenters()
+        {
+            return new List<Presenter>
+            {
+                new Presenter{Id = 1, Name = "Al Alberts",   Bio = "AAA", CreatedAt = DateTime.Now },
+                new Presenter{Id = 2, Name = "Bob Baker",    Bio = "BBB", CreatedAt = DateTime.Now },
+                new Presenter{Id = 3, Name = "Carla Carlos", Bio = "CCC", CreatedAt = DateTime.Now },
+            };
+        }
+
+        public static IList<PresenterResponseDto> MockPresentersResponse()
+        {
+            return new List<PresenterResponseDto>
+            {
+                new PresenterResponseDto{Id = 1, Name = "Al Alberts",   Bio = "AAA", CreatedAt = DateTime.Now },
+                new PresenterResponseDto{Id = 2, Name = "Bob Baker",    Bio = "BBB", CreatedAt = DateTime.Now },
+                new PresenterResponseDto{Id = 3, Name = "Carla Carlos", Bio = "CCC", CreatedAt = DateTime.Now },
+            };
+        }
+
+
+
+        public static PresenterPostDto MockPresenterPostDto()
+        {
+            return new PresenterPostDto { Name = "Dave Davis", Bio = "DDD" };
+
+        }
+        public static Presenter MockPresenterAdded()
+        {
+            return new Presenter { Id = 4, Name = "Dave Davis", Bio = "DDD" };
+
+        }
+    }
+}
--- NET6.WebApi.Test/PresentersControllerTests.cs
+++ NET6.WebApi.Test/PresentersControllerTests.cs
@@ -5,8 +5,8 @@ using Moq;
 using NET6.Shared.Dtos;
 using NET6.Shared.Models;
 using NET6.WebApi.Controllers;
-using NET6.WebApi.MappingProfiles;
 using NET6.WebApi.Repositories;
+using NET6.WebApi.Test.MockData;
 using System.Collections.Generic;
 using Xunit;
 
@@ -16,31 +16,28 @@ namespace NET6.WebApi.Test
     {
         private readonly Mock<ILogger<PresentersController>> _logger;
         private readonly Mock<IPresentersRepository> _mockRepo;
+        private readonly Mock<IMapper> _mockMapper;
         private readonly PresentersController _controller;
-        private readonly IMapper _mapper;
-
 
         public PresentersControllerTests()
         {
             _logger = new Mock<ILogger<PresentersController>>();
             _mockRepo = new Mock<IPresentersRepository>();
+            _mockMapper = new Mock<IMapper>();
 
-            // AutoMapper
-            var config = new MapperConfiguration(cfg => cfg.AddMaps(new[] { typeof(RepositoryMappingProfiles) }));
-            _mapper = new Mapper(config);
-            _controller = new PresentersController(_logger.Object, _mockRepo.Object, _mapper);
+            _controller = new PresentersController(_logger.Object, _mockRepo.Object, _mockMapper.Object);
         }
 
         [Fact]
         public async void GetAll_ReturnsExactCount()
         {
             // Arrange
-            _mockRepo.Setup((repo) => repo.GetAllAsync(null))
-                .ReturnsAsync(new List<Presenter>
-                {
-                    new Presenter{},
-                    new Presenter{}
-                });
+            var presenters = PresentersMockData.MockPresenters();
+            _mockRepo.Setup(repo => repo.GetAllAsync(null))
+                .ReturnsAsync(presenters);
+
+            _mockMapper.Setup(mapper => mapper.Map<IList<PresenterResponseDto>>(presenters))
+                .Returns(PresentersMockData.MockPresentersResponse());
 
             // Act
             var result = await _controller.GetAll(null);
@@ -50,26 +47,30 @@ namespace NET6.WebApi.Test
             var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
             var apiResponse = Assert.IsType<ApiResponse<IList<PresenterResponseDto>>>(okResult.Value);
             Assert.True(apiResponse.Success);
-            Assert.Equal(2, apiResponse.Data?.Count);
+            Assert.Equal(3, apiResponse.Data?.Count);
         }
 
         [Fact]
-        public async void Create_Returns_CreatedAtAction()
+        public async void Add_Returns_CreatedAtAction()
         {
             // Arrange
-            var presenter = new Presenter();
-            _mockRepo.Setup((repo) => repo.AddAsync(presenter));
+            var presenterDto = new PresenterPostDto();
+            var presenter = PresentersMockData.MockPresenterAdded();
+            _mockRepo.Setup(repo => repo.AddAsync(presenter))
+                            .ReturnsAsync(presenter.Id);
+
+            _mockMapper.Setup(mapper => mapper.Map<Presenter>(presenterDto))
+                .Returns(presenter);
 
             // Act
-            var presenterDto = new PresenterPostDto();
             var result = await _controller.Add(presenterDto);
 
             // Assert
-            var actionResult = Assert.IsType<ActionResult<ApiResponse<int>>>(result);
-            var createdAtResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+            var createdAtResult = Assert.IsType<CreatedAtActionResult>(result.Result);
             var apiResponse = Assert.IsType<ApiResponse<int>>(createdAtResult.Value);
             Assert.Equal("getbyid", createdAtResult.ActionName?.ToLower());
-            //Assert.Equal("Id", createdAtResult.)
+            Assert.Equal("Id", (createdAtResult.RouteValues.Keys as string[])[0]);
+            Assert.Equal(apiResponse.Data, (createdAtResult.RouteValues.Values as object[])[0]);
         }
     }
 }
